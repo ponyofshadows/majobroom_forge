7dcd3e59f2f2d1cc118ddbab3b1408175194db31
Polaris_Light <49135577+GLDYM@users.noreply.github.com>
2024-11-04 20:57:04 +0800

    Update JsonBroomModel.java

--- src/main/java/com/innky/majobroom/entity/model/JsonBroomModel.java
+++ src/main/java/com/innky/majobroom/entity/model/JsonBroomModel.java
@@ -7,7 +7,6 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
