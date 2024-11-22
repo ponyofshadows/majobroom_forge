48f9e95684b5a5f58648e9a5c482c6b0dfba8683
Polaris_Light <49135577+GLDYM@users.noreply.github.com>
2024-11-22 11:11:29 +0800

    Update MajoWearableItem.java

--- src/main/java/com/innky/majobroom/armors/MajoWearableItem.java
+++ src/main/java/com/innky/majobroom/armors/MajoWearableItem.java
@@ -10,6 +10,7 @@ import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.DyeableLeatherItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
